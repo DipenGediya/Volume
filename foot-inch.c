0deb966a7b608f48901f47c4d204c43abf6f80f8
DipenGediya <141924820+DipenGediya@users.noreply.github.com>
2024-01-03 22:53:21 +0530

    Update foot-inch.c

--- foot-inch.c
+++ foot-inch.c
@@ -3,10 +3,10 @@
 struct Distance {
    int feet;
    float inch;
-} d1, d2, result;
+}
 
 main() {
-  
+  	struct Distance d1, d2, result;
    printf("Enter 1st distance\n");
    printf("Enter feet: ");
    scanf("%d", &d1.feet);
@@ -26,6 +26,6 @@ main() {
       result.inch = result.inch - 12.0;
       ++result.feet;
    }
-   printf("\nSum of distances = %d\'-%.1f\"", result.feet, result.inch);
+   printf("\nSum of distances = %d\'%.1f\"", result.feet, result.inch);
 
 }
